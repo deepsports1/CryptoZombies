db200916b4d873336218929eba0e92588c05a12b
Josh Reback <deepsports1610@gmail.com>
2021-11-17 00:13:28 -0500

    fix failing test

--- test/helpers/time.js
+++ test/helpers/time.js
@@ -1,21 +1,23 @@
 async function increase(duration) {
-
-    //first, let's increase time
-    await web3.currentProvider.send({
-        jsonrpc: "2.0",
-        method: "evm_increaseTime",
-        params: [duration], // there are 86400 seconds in a day
-        id: new Date().getTime()
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.send({
+            jsonrpc: "2.0",
+            method: "evm_increaseTime",
+            params: [duration],
+            id: new Date().getTime()
+        }, (err, result) => {
+            // second call within the callback
+            web3.currentProvider.send({
+                jsonrpc: '2.0',
+                method: 'evm_mine',
+                params: [],
+                id: new Date().getTime()
+            }, (err, result) => {
+                // need to resolve the Promise in the second callback
+                resolve();
+            });
+        });
     });
-
-    //next, let's mine a new block
-    web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_mine',
-        params: [],
-        id: new Date().getTime()
-    });
-
 }
 
 const duration = {
